78d7b29d2abf950ee9e55ab383239d656feec0dd
balkan-power <dimitar.ivanov@student.gildeopleidingen.nl>
2026-07-07 10:12:50 +0200

    Update clock.js

--- static/js/clock.js
+++ static/js/clock.js
@@ -1,17 +1,19 @@
 function updateClock() {
-        const now = new Date();
-        const pad = n => String(n).padStart(2, "0");
+    // setting it inside home.go only sets it once, therefore
+    // not a live clock. no choice but to use j*v*scr*pt
+    const now = new Date();
+    const pad = n => String(n).padStart(2, "0");
 
-        // ai slop
-        document.getElementById("clock").textContent =
-        "Today is: " +
-        `${pad(now.getDate())}.` +
-        `${pad(now.getMonth() + 1)}.` +
-        `${now.getFullYear()} ` +
-        `${pad(now.getHours())}:` +
-        `${pad(now.getMinutes())}:` +
-        `${pad(now.getSeconds())}`;
-    }
+    // ai slop
+    document.getElementById("clock").textContent =
+    "Today is: " +
+    `${pad(now.getDate())}.` +
+    `${pad(now.getMonth() + 1)}.` +
+    `${now.getFullYear()} ` +
+    `${pad(now.getHours())}:` +
+    `${pad(now.getMinutes())}:` +
+    `${pad(now.getSeconds())}`;
+}
 
-    updateClock();
-    setInterval(updateClock, 1000); // update once every second (i hate js)
+updateClock();
+setInterval(updateClock, 1000); // update once every second (i hate js)
